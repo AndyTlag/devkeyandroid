a122d8b80e360f6cd25d05a1dd664e75ab081ed0
Rubens Leme <rubenss.leme@gmail.com>
2019-11-12 12:05:22 -0300

    botoes inseridos, menos cliente do menu

--- app/src/main/java/com/example/devkey/Cliente.java
+++ app/src/main/java/com/example/devkey/Cliente.java
@@ -7,30 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Cliente extends AppCompatActivity {
 
-    Button btn_membro;
+    Button btn_cad_cli;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cliente);
         inicializarObjetos();
     }
 
     public void inicializarObjetos(){
-        btn_membro = (Button) findViewById(R.id.btn_cad_mb);
+        btn_cad_cli = (Button) findViewById(R.id.btn_cad_mb);
         abrirActivityCadMb();
     }
 
     public void abrirActivityCadMb(){
-        btn_membro.setOnClickListener(new View.OnClickListener(){
+        btn_cad_cli.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Membro.class);
+                intent.setClass(Cliente.this, CadCliente.class);
             }
         })
     }
-
 }
