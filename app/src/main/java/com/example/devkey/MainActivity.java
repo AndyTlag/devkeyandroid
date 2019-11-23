95fc4bc1bee124282b190cde4b079eaa3d9f9a4d
Rubens Leme <rubenss.leme@gmail.com>
2019-11-22 22:34:44 -0300

    pelo menos alguma coisa eu fiz

--- app/src/main/java/com/example/devkey/MainActivity.java
+++ app/src/main/java/com/example/devkey/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.devkey;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/devkey/SplashDevMedia.java
+++ app/src/main/java/com/example/devkey/SplashDevMedia.java
@@ -1,35 +1,32 @@
 package com.example.devkey;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import com.example.devkey.ui.login.LoginActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.devkey.ui.login.LoginActivity;
 
 public class SplashDevMedia extends Activity {
-    // Timer da splash screen
-    private static int SPLASH_TIME_OUT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            /*
-             * Exibindo splash com um timer.
-             */
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Esse método será executado sempre que o timer acabar
-                // E inicia a activity principal
-                Intent i = new Intent(SplashDevMedia.this,
-                        LoginActivity.class);
-                startActivity(i);
-
-                // Fecha esta activity
-                finish();
+                mostrarLoginActivity();
             }
-        }, SPLASH_TIME_OUT);
+        }, 2000);
+    }
+
+    private void mostrarLoginActivity() {
+        Intent intent = new Intent(
+                SplashDevMedia.this, LoginActivity.class
+        );
+        startActivity(intent);
+        finish();
     }
 }
